f2ff476e4d1534457bcc01fe2f9f2f13b63e3ab7
JuanEstrada01 <79321459+JuanEstrada01@users.noreply.github.com>
2022-05-01 16:27:32 -0500

    Se crea el movimiento de la nave

--- Phaser-game/src/scenes/playScene.js
+++ Phaser-game/src/scenes/playScene.js
@@ -17,11 +17,32 @@ export default class PlayScene extends Phaser.Scene{
         this.add.image(0, 480, 'background');
         this.add.image(640, 480, 'background');
         this.ship = this.physics.add.image(400, 300, 'ship');
+        //velocidad y control de la nave
+        this.ship.setDrag(0.99);
+        this.ship.setMaxVelocity(200);
+        this.ship.setCollideWorldBounds(true);
+        this.ship.setSize(20, 30);
+        this.cursors = this.input.keyboard.createCursorKeys();
+
 
        
 
     }
     update(){
+        //se crean las teclas movimiento
+        if(this.cursors.up.isDown){
+            this.physics.velocityFromRotation(this.ship.rotation,200,this.ship.body.acceleration);
+        }else{
+            this.ship.setAcceleration
+        }
+        if(this.cursors.left.isDown){
+            this.ship.setAngularVelocity(-300);
+        }else if(this.cursors.right.isDown){
+            this.ship.setAngularVelocity(300);
+        }else{
+            this.ship.setAngularVelocity(0);
+        }
+
 
     }
         
