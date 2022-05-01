2111d432c53ca1b6c3432231cb6429386ea81d8b
JuanEstrada01 <79321459+JuanEstrada01@users.noreply.github.com>
2022-05-01 13:20:35 -0500

    se agregan las imagenes en la escena

--- Phaser-game/src/index.js
+++ Phaser-game/src/index.js
@@ -1,18 +1,18 @@
 // se importa la escena del juego
-import PlayScene from "./scenes/playScene.js";
-const config = {
-width: 800,
-height: 600,
-type: Phaser.Auto,
-//motor de fisicas 
-phsysics: {
-    default : "arcade",
-    arcade: {
-        fps:60,
-        gravity: {y : 0}
-    }
+import PlayScene from './scenes/playscene.js';
 
-},
-scene: [PlayScene]
-};
+const config = {
+    width: 800,
+    height: 600,
+    type: Phaser.AUTO,
+    physics:{
+        default: "arcade",
+        arcade: {
+            fps: 60,
+            gravity: { y: 0 }
+        }
+    },
+    scene: [PlayScene]
+}
+
 const game = new Phaser.Game(config);
--- Phaser-game/src/scenes/playScene.js
+++ Phaser-game/src/scenes/playScene.js
@@ -5,12 +5,27 @@ export default class PlayScene extends Phaser.Scene{
     }
     //configuracion de metodos
     preload(){
-        console.log('PlayScene');
+       this.load.image('background', '../../assets/img/background.png');
+       this.load.image('ship', './img/ship.png');
+       this.load.image('asteroid-1', './img/asteroid-1.png');
+       this.load.image('shoot', './img/shoot.png');
+
     }
     create(){
+        this.add.image(0, 0, 'background');
+        this.add.image(640, 0, 'background');
+        this.add.image(0, 480, 'background');
+        this.add.image(640, 480, 'background');
+        this.ship = this.physics.add.image(400, 300, 'ship');
+
+       
 
     }
     update(){
 
     }
+        
+       
+
+    
 }
